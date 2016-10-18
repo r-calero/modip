5ee30513e1d94d64a1914b84d1dc7e34b67757ca
Dany Naranjo <danynaranjofeliciano@gmail.com>
2016-10-18 14:47:48 +0000

    Update notifier.rb

--- PIAM/app/mailers/notifier.rb
+++ PIAM/app/mailers/notifier.rb
@@ -1,5 +1,5 @@
 class Notifier < ActionMailer::Base
-  default :from => "modipserver@gmail.com"
+  default :from => "someone@gmail.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
