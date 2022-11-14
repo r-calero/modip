f7d482a752d5660cb2ccde458fc1af25f3395b48
Dany Naranjo Feliciano <77994691+danynf@users.noreply.github.com>
2022-11-14 11:19:31 -0600

    Update database_configuration.rb
    Fix db connection.

--- Server/database_configuration.rb
+++ Server/database_configuration.rb
@@ -2,7 +2,7 @@ require 'active_record'
 require 'active_support'
 
 ActiveRecord::Base.establish_connection(
-  :adapter => "mysql",
+  :adapter => "mysql2",
   :host => "127.0.0.1",
   :database => "censa",
   :username => "admindemas",
